8747cf854ad7d4d113487d069636ea751236fd31
Alan van Dam <alan@vandamnet.nl>
2014-03-04 15:43:03 +0100

    Corrected namespace service type

--- src/main/java/net/es/nsi/pce/services/Service.java
+++ src/main/java/net/es/nsi/pce/services/Service.java
@@ -1,42 +1,43 @@
 package net.es.nsi.pce.services;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * This class is a temporary solution for the serviceType to service element
  * mapping that will be contained in the service description file.
- * 
+ *
  * @author hacksaw
  */
 public enum Service {
     P2PS("{http://schemas.ogf.org/nsi/2013/12/services/point2point}p2ps"),
     UNSUPPORTED("unsupported");
-    
+
     // The QNAME associated with this enum instance.
     private final String qname;
-    
+
     // Service mapping for A-GOLE service type.
-    final static String EVTS_AGOLE = "http://services.ogf.org/nsi/2013/07/descriptions/EVTS.A-GOLE";
-    
+    final static String EVTS_AGOLE = "http://services.ogf.org/nsi/2013/12/descriptions/EVTS.A-GOLE";
+
     // Lists of service elements associated with the EVTS AGOLE service.
     private final static List<Service> EVTS_AGOLE_LIST = new ArrayList<Service>() {
         private static final long serialVersionUID = 1L;
         { add(P2PS); }
     };
-    
+
     // Master list of supported services and mappings to elements.
     private final static HashMap<String, List<Service>> typeToServiceMap = new HashMap<String, List<Service>>() {
         private static final long serialVersionUID = 1L;
-        { 
+        {
             put(EVTS_AGOLE, EVTS_AGOLE_LIST);
         }
     };
-    
+
     /**
      * Constructor to create enum members with string mapping to XML QNAME.
-     * 
+     *
      * @param text XML QNAME for service element.
      */
     private Service(final String qname) {
@@ -45,17 +46,17 @@ public enum Service {
 
     /**
      * Return string QNAME for the enum instead of a name value.
-     * 
+     *
      * @see java.lang.Enum#toString()
      */
     @Override
     public String toString() {
         return qname;
     }
-    
+
     /**
      * Get the QNAME associated with the enum element.
-     * 
+     *
      * @return QNAME of the enum element.
      */
     public String getQname() {
@@ -64,7 +65,7 @@ public enum Service {
 
     /**
      * Get the Service enum associated with the provided QNAME.
-     * 
+     *
      * @param qname XML namespace QNAME associated with the service element.
      * @return Service corresponding to QNAME.
      */
@@ -75,18 +76,23 @@ public enum Service {
                 return values[i];
             }
         }
-        
+
         return UNSUPPORTED;
     }
-    
+
     /**
      * Get the Service associated with the string serviceType.
-     * 
+     *
      * @param type String serviceType associated with the supported service.
-     * 
+     *
      * @return List of Service elements associated with serviceType.
      */
     public static List<Service> getServiceByType(String type) {
-        return typeToServiceMap.get(type);
+        if (typeToServiceMap.containsKey(type)) {
+            return typeToServiceMap.get(type);
+        }
+        else {
+          return Collections.emptyList();
+        }
     }
 }
