3a1ecd163d194187a6727cd37e0acf3f9ad53547
Alan van Dam <alan@vandamnet.nl>
2014-04-01 23:02:39 +0200

    Added reachability data structure to PCEData

--- src/main/java/net/es/nsi/pce/pf/api/PCEData.java
+++ src/main/java/net/es/nsi/pce/pf/api/PCEData.java
@@ -4,27 +4,29 @@ import net.es.nsi.pce.pf.api.cons.Constraint;
 import net.es.nsi.pce.topology.model.NsiTopology;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Defines the data passed into and between Path Computation Modules.
- * 
+ *
  * path - contains the computed path so far.  Each PC module can further
  * refine the path for the next PC module in the sequence.
- * 
+ *
  * constraints - contains list of constraints to guide path finding.  Constraints
  * will contain user requested parameters such as source and destination STP,
  * as well as vlan tags, ERO, etc.  It can also contain additional constraints
  * added by the system.
- * 
+ *
  * topology - the current network topology utilized for pathfinding.
- * 
+ *
  * @author hacksaw
  */
 public class PCEData {
     private Path path = new Path();
     private Set<Constraint> constraints = new HashSet<>();
     private NsiTopology topology = new NsiTopology();
+    private Map<String, Map<String, Long>> reachabilityTable;
 
     public Path getPath() {
         return path;
@@ -44,5 +46,13 @@ public class PCEData {
 
     public void setTopology(NsiTopology topology) {
         this.topology = topology;
+    }
+
+    public Map<String, Map<String, Long>> getReachabilityTable() {
+        return reachabilityTable;
+    }
+
+    public void setReachabilityTable(Map<String, Map<String, Long>> reachabilityTable) {
+        this.reachabilityTable = reachabilityTable;
     }
 }
