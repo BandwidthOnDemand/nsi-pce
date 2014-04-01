aff9f2bc0cf1e856bf5da055ef8f18b1bf339575
Alan van Dam <alan@vandamnet.nl>
2014-04-01 23:02:10 +0200

    Adding some functionality to ReachabilityPCE

--- src/main/java/net/es/nsi/pce/pf/ReachabilityPCE.java
+++ src/main/java/net/es/nsi/pce/pf/ReachabilityPCE.java
@@ -1,13 +1,88 @@
 package net.es.nsi.pce.pf;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
+
+import net.es.nsi.pce.path.services.Point2Point;
+import net.es.nsi.pce.pf.api.NsiError;
 import net.es.nsi.pce.pf.api.PCEData;
 import net.es.nsi.pce.pf.api.PCEModule;
+import net.es.nsi.pce.pf.api.Path;
+import net.es.nsi.pce.pf.api.cons.Constraints;
+import net.es.nsi.pce.pf.api.cons.StringAttrConstraint;
 
 public class ReachabilityPCE implements PCEModule {
 
     @Override
     public PCEData apply(PCEData pceData) {
+        Optional<Path> path = findPath(pceData);
+
+        if (path.isPresent()) {
+            pceData.setPath(path.get());
+        }
+
         return pceData;
+    }
+
+    protected Optional<Path> findPath(PCEData pceData) {
+        Constraints constraints = new Constraints(pceData.getConstraints());
+
+        String sourceStp = getSourceStp(constraints);
+        String destStp = getDestinationStp(constraints);
+
+        String sourceTopologyId = extractTopologyIdOrFail(sourceStp, Point2Point.SOURCESTP);
+        String destTopologyId = extractTopologyIdOrFail(destStp, Point2Point.DESTSTP);
+
+        return Optional.absent();
+    }
+
+
+    private String getSourceStp(Constraints constraints) {
+        return getStringValue(Point2Point.SOURCESTP, constraints);
+    }
+
+    private String getDestinationStp(Constraints constraints) {
+        return getStringValue(Point2Point.DESTSTP, constraints);
+    }
+
+    private String getStringValue(String attributeName, Constraints constraints) {
+        Optional<String> value = getValue(constraints.getStringAttrConstraint(attributeName));
+
+        if (value.isPresent()) {
+            return value.get();
+        }
+
+        throw new IllegalArgumentException(NsiError.getFindPathErrorString(NsiError.MISSING_PARAMETER, Point2Point.NAMESPACE, attributeName));
+    }
+
+    private Optional<String> getValue(StringAttrConstraint constraint) {
+        if (constraint == null) {
+            return Optional.absent();
+        }
+
+        return Optional.fromNullable(Strings.emptyToNull(constraint.getValue()));
+    }
+
+    private String extractTopologyIdOrFail(String stpId, String attributeName) {
+        Optional<String> topologyId = extractTopologyId(stpId);
+        if (topologyId.isPresent()) {
+            return topologyId.get();
+        }
+
+        throw new IllegalArgumentException(NsiError.getFindPathErrorString(NsiError.STP_RESOLUTION_ERROR, Point2Point.NAMESPACE, attributeName));
+    }
+
+    protected Optional<String> extractTopologyId(String stpId) {
+        checkNotNull(stpId);
+
+        Iterable<String> parts = Iterables.limit(Splitter.on(":").split(stpId), 5);
+
+        return Iterables.size(parts) == 5 ? Optional.of(Joiner.on(":").join(parts)) : Optional.<String>absent();
     }
 
 }
--- src/test/java/net/es/nsi/pce/pf/ReachabilityPCETest.java
+++ src/test/java/net/es/nsi/pce/pf/ReachabilityPCETest.java
@@ -1,8 +1,15 @@
 package net.es.nsi.pce.pf;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.base.Optional;
+
 import net.es.nsi.pce.pf.api.PCEData;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -10,11 +17,53 @@ public class ReachabilityPCETest {
 
     private ReachabilityPCE subject = new ReachabilityPCE();
 
-    @Test
-    public void shouldDoNothingYet() throws Exception {
-        PCEData initialData = new PCEData();
-        PCEData resultingData = subject.apply(initialData);
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_when_missing_source_stp() {
+        PCEData pceData = new PCEData();
+        subject.findPath(pceData);
+    }
 
-        assertEquals(initialData, resultingData);
+    @Test
+    @Ignore("todo")
+    public void should_calculate_path_if_both_src_and_dest_is_in_topology() {
+    }
+
+    @Test
+    @Ignore("todo")
+    public void should_splitup_request_if_src_is_in_toplogy() {
+    }
+
+    @Test
+    @Ignore("todo")
+    public void should_forward_request_to_nearest_nsa() {
+    }
+
+    @Test
+    public void should_find_topology_id_from_stp_id() {
+        List<String> stpIds = Arrays.asList(
+            "urn:ogf:network:surfnet.nl:1990:topology:foobar",
+            "urn:ogf:network:surfnet.nl:1990:testbed:1",
+            "urn:ogf:network:surfnet.nl:1990:testbed:foobar",
+            "urn:ogf:network:surfnet.nl:1990:testbed:foo:bar",
+            "urn:ogf:network:surfnet.nl:1990:testbed");
+
+        for (String stpId : stpIds) {
+            Optional<String> topologyId = subject.extractTopologyId(stpId);
+
+            assertTrue(topologyId.isPresent());
+            assertEquals("urn:ogf:network:surfnet.nl:1990", topologyId.get());
+        }
+    }
+
+    @Test
+    public void should_not_find_topology_id_in_empty_string() {
+        Optional<String> topologyId = subject.extractTopologyId("");
+
+        assertFalse(topologyId.isPresent());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void should_throw_nullpointer_when_stp_id_is_null() {
+        subject.extractTopologyId(null);
     }
 }
